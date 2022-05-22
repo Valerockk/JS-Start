1eb78ee53d36dcd004b3a8e084b19131849e2d04
Valerockk <trojan.valerii@gmail.com>
2022-05-22 18:50:11 +0300

    first test message

---
+++ js/script.js
@@ -0,0 +1,96 @@
+"use strict";
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyWidthLength = 4000;
+
+// console.log("Ширина кузова: " + vehicleBodyWidth + ", довжина:" + vehicleBodyWidthLength);
+
+// ['kbjv', 'kbjvskv', 'kvjbhek'].map(item => );
+
+//Request
+// data 
+// response 
+
+// camelCase
+// snake_case
+// UPPER_SNAKE_CASE
+
+// let number = 4.6;
+
+// console.log(-4/0); // -Infinity
+// console.log('string' * 9); // NaN
+// // console.log(something); // -Infinity
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false,
+// };
+// console.log(obj.name); //John
+// // console.log(obj["name"]); //John
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// const arr = ['a', 'b', 'c'];
+// let arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c',
+//     abc:  {
+//         df: [{}, {}],
+//         def: {
+
+//         }
+//     }
+// };
+// arrObj.b = '31412';
+
+// console.log(arrObj);
+// console.log(arrObj.b);
+
+
+
+// // const obj = {a: 1, b: 2};
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+// alert('Hello!');
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "введите ответ");
+// console.log(typeof (answer));
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Напишить будь ласка ваше ім`я?', '');
+// answers[1] = prompt('Як ваше прізвище?', '');
+// answers[2] = prompt('Скільки вам років ?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category + '/' + '4');
+// console.log(`https://someurl.com/${category}/4`);
+
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+console.log(2 * 4 == '8');
+console.log(2 * 4 === '8');
+
+const isChecked = false,
+    isClose = false;
+
+
+console.log(isChecked || !isClose);
